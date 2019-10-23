ef265d2e4472b67971c08005d759c8a0a1dbfcd7
Diego Brusa <diego.brusa@fizzmod.com>
2019-10-23 14:35:58 -0300

    Generated base package

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { MeliAuthSdk } = require('./lib');
+
+module.exports = MeliAuthSdk;
---
+++ tests/meli-auth-sdk-test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// const assert = require('assert');
+
+// const sandbox = require('sinon').createSandbox();
+
+// const MeliAuthSdk = require('./../index');
+
+// const MeliAuthSdkError = require('./../lib/meli-auth-sdk-error');
+
+describe('MeliAuthSdk', () => {
+
+	// your tests here...
+
+});
